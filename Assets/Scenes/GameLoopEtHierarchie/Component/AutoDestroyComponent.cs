22d9d5925fb7c499692c9618f6f428de60c48bc4
adytekk <adryan.hauton@gmail.com>
2020-10-12 00:39:21 +0200

    HomePush

---
+++ Assets/Scenes/GameLoopEtHierarchie/Component/AutoDestroyComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroyComponent : MonoBehaviour
+{
+
+    public float TimeBeforeDestroy = 2.0f;
+
+    void Start()
+    {
+        Invoke("DestroyThis", TimeBeforeDestroy);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scenes/GameLoopEtHierarchie/Component/FireComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireComponent : MonoBehaviour
+{
+
+    public GameObject AmmoPrefab;
+    public int FirePower;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Fire()
+    {
+        GameObject ammoShot = Instantiate(AmmoPrefab, transform.position, Quaternion.identity);
+        ammoShot.GetComponent<Rigidbody>().AddForce(transform.right * FirePower, ForceMode.Impulse);
+    }
+
+}
